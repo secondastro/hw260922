f5c5ddd6525065a5f0ffb1601e383fef15618190
User <anton_trenogin@mail.ru>
2022-09-30 15:04:00 +0500

    Сделал минимальный уровень

--- src/Work.java
+++ src/Work.java
@@ -2,18 +2,19 @@ public class Work {
     public static void main(String[] args) {
 
         Book shine = new Book("Сияние",1973, new Author("Стивен", "Кинг"));
-        System.out.print(shine.getName() + ": ");
-        System.out.print(shine.getAuthor().getFirstName() + " " + shine.getAuthor().getLastName() + ": ");
-        System.out.print(shine.getYear());
-        System.out.println();
+        printerBook(shine);
 
         Book oprichniksDay = new Book("День опричника", 2006, new Author("Владимир", "Сорокин"));
-        System.out.print(oprichniksDay.getName() + ": ");
-        System.out.print(oprichniksDay.getAuthor().getFirstName() + " " + oprichniksDay.getAuthor().getLastName() + ": ");
-        System.out.print(oprichniksDay.getYear());
-        System.out.println();
+        printerBook(oprichniksDay);
 
         oprichniksDay.setYear(2007);
         System.out.println(oprichniksDay.getYear());
+    }
+
+    public static void printerBook(Book book) {
+        System.out.print(book.getName() + ": ");
+        System.out.print(book.getAuthor().getFirstName() + " " + book.getAuthor().getLastName() + ": ");
+        System.out.print(book.getYear());
+        System.out.println();
     }
 }
